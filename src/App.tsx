ef9db09986a7163df16fdb68d5fae970c9ac18e6
Rehan-shah <Rehansnehalshah1@gmail.com>
2024-04-23 19:04:41 +0530

    adding anylastic feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
 import React, { useState } from "react";
 import "./index.css";
 import Screen from "./screen";
-
+import { Analytics } from "@vercel/analytics/react"
 const App = () => {
-  const [selectedChapter, setSelectedChapter] = useState(1);
+    const [selectedChapter, setSelectedChapter] = useState(1);
 
-  const handleChapterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedChapter(parseInt(event.target.value));
-  };
+    const handleChapterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedChapter(parseInt(event.target.value));
+    };
 
-  return (
-    <div className="app">
-      <div className="dropdown-container">
-        <label htmlFor="chapter-select">Select Chapter:</label>
-        <select
-          id="chapter-select"
-          value={selectedChapter}
-          onChange={handleChapterChange}
-        >
-          {Array.from(
-            { length: 14 },
-            (_, index) => (
-              <option key={index + 1} value={index + 1}>
-                Chapter {index + 1}
-              </option>
-            ),
-          )}
-        </select>
-      </div>
-      <Screen name_chapter={`Chapter ${selectedChapter}`} />
-    </div>
-  );
+    return (
+        <>
+            <div className="app">
+                <div className="dropdown-container">
+                    <label htmlFor="chapter-select">Select Chapter:</label>
+                    <select
+                        id="chapter-select"
+                        value={selectedChapter}
+                        onChange={handleChapterChange}
+                    >
+                        {Array.from(
+                            { length: 14 },
+                            (_, index) => (
+                                <option key={index + 1} value={index + 1}>
+                                    Chapter {index + 1}
+                                </option>
+                            ),
+                        )}
+                    </select>
+                </div>
+                <Screen name_chapter={`Chapter ${selectedChapter}`} />
+            </div>
+            <Analytics />
+        </>
+    );
 };
 
 export default App;
