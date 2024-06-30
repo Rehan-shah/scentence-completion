ff246f34a87f0becc34d07492fa1adc5a213e6ed
Rehan-shah <Rehansnehalshah1@gmail.com>
2024-06-30 16:25:55 +0530

    fix: bug where question num is no correct

--- src/Question.tsx
+++ src/Question.tsx
@@ -3,81 +3,92 @@ import { useState } from "react";
 import "./App.css";
 
 const Question = (
-  { question, options, answer }: {
-    question: string;
-    options: any;
-    answer: string;
-  },
+    { question, options, answer, q_num }: {
+        question: string;
+        options: any;
+        answer: string;
+        q_num: string
+    },
 ) => {
-  const [showMeanings, setShowMeanings] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("");
-  const [isAnswerCorrect, setIsAnswerCorrect] = useState<null | boolean>(null);
-  console.log(options);
-  const toggleMeanings = () => {
-    setShowMeanings(!showMeanings);
-  };
+    const [showMeanings, setShowMeanings] = useState(false);
+    const [selectedOption, setSelectedOption] = useState("");
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState<null | boolean>(null);
+    console.log(options);
+    const toggleMeanings = () => {
+        setShowMeanings(!showMeanings);
+    };
 
-  const handleOptionChange = (option: any) => {
-    setSelectedOption(option);
-    setIsAnswerCorrect(null); // Reset the answer indicator when selecting a new option
-  };
+    const handleOptionChange = (option: any) => {
+        console.log(option);
+        setSelectedOption(option);
+        setIsAnswerCorrect(null); // Reset the answer indicator when selecting a new option
+    };
 
-  const checkAnswer = () => {
-    if (isAnswerCorrect === null) {
-      const isCorrect = selectedOption === answer;
-      setIsAnswerCorrect(isCorrect);
-      localStorage.setItem(
-        question,
-        JSON.stringify({
-          "selectedAnswer": selectedOption,
-          "previous_try": isCorrect,
-        }),
-      );
-    } else {
-      setIsAnswerCorrect(null);
+    const checkAnswer = () => {
+        if (isAnswerCorrect === null) {
+            const isCorrect = selectedOption === answer;
+            setIsAnswerCorrect(isCorrect);
+            localStorage.setItem(
+                question,
+                JSON.stringify({
+                    "selectedAnswer": selectedOption,
+                    "previous_try": isCorrect,
+                }),
+            );
+        } else {
+            setIsAnswerCorrect(null);
+        }
+    };
+
+    question = question.replace(/\d+/g, '')
+
+    if (question[1] === ".") {
+        question = question.slice(2);
+    } else if (question[0] == ".") {
+        question = question.slice(1);
     }
-  };
 
-  return (
-    <div className="question-container">
-      <p className="question">{question}</p>
-      {Object.keys(options).map((key) => (
-        <div key={key} className="option-container">
-          <label>
-            <input
-              type="radio"
-              name="option"
-              value={key}
-              checked={selectedOption === key}
-              onChange={() =>
-                handleOptionChange(key)}
-            />{" "}
-            <span className="option">{options[key].option}</span>
-          </label>
-          {showMeanings && (
-            <p className="meaning">
-              <strong>Meaning:</strong> {options[key].meaning}
-            </p>
-          )}
+
+    console.log(q_num)
+    return (
+        <div className="question-container">
+            <p className="question">{q_num}) {question}</p>
+            {Object.keys(options).map((key) => (
+                <div key={key} className="option-container">
+                    <label>
+                        <input
+                            type="radio"
+                            name="option"
+                            value={key}
+                            checked={selectedOption === key}
+                            onChange={() =>
+                                handleOptionChange(key)}
+                        />{" "}
+                        <span className="option">{options[key].option}</span>
+                    </label>
+                    {showMeanings && (
+                        <p className="meaning">
+                            <strong>Meaning:</strong> {options[key].meaning}
+                        </p>
+                    )}
+                </div>
+            ))}
+            <button className="toggle-btn" onClick={toggleMeanings}>
+                Toggle Meanings
+            </button>
+            <button className="check-btn" onClick={checkAnswer}>
+                Check Answer
+            </button>
+            {isAnswerCorrect !== null && (
+                <p
+                    className={`answer-indicator ${isAnswerCorrect ? "correct" : "incorrect"
+                        }`}
+                >
+                    {isAnswerCorrect ? "Correct!" : "Incorrect!"}
+                </p>
+            )}
         </div>
-      ))}
-      <button className="toggle-btn" onClick={toggleMeanings}>
-        Toggle Meanings
-      </button>
-      <button className="check-btn" onClick={checkAnswer}>
-        Check Answer
-      </button>
-      {isAnswerCorrect !== null && (
-        <p
-          className={`answer-indicator ${
-            isAnswerCorrect ? "correct" : "incorrect"
-          }`}
-        >
-          {isAnswerCorrect ? "Correct!" : "Incorrect!"}
-        </p>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Question;
--- src/screen.tsx
+++ src/screen.tsx
@@ -4,56 +4,59 @@ import Question from "./Question";
 import LastQuestion from "./LastQuestion";
 
 function Screen({ name_chapter }: any) {
-  const [isShowingHistory, setShowingHistory] = useState(false);
-  function giveOptions(json: any) {
-    const hi = {
-      "a": json["a"],
-      "b": json["b"],
-      "c": json["c"],
-      "d": json["d"],
-      "e": json["e"],
-    };
-    return hi;
-  }
-  let data: any = Data;
-  return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>{name_chapter}</h1>{" "}
-      <button
-        style={{
-          marginRight: "auto",
-          marginLeft: "auto",
-          backgroundColor: "tan",
-        }}
-        onClick={() => {
-          setShowingHistory(!isShowingHistory);
-        }}
-      >
-        Show previous
-      </button>
-      <div style={{ width: "50%", margin: "auto" }}>
-        {Object.keys(data[name_chapter]).map((qes, i) => (
-          !isShowingHistory
-            ? (
-              <Question
-                key={i}
-                question={data[name_chapter][qes]["Question"]}
-                options={giveOptions(data[name_chapter][qes])}
-                answer={data[name_chapter][qes]["Answer"][0]}
-              />
-            )
-            : (
-              <LastQuestion
-                key={i}
-                question={data[name_chapter][qes]["Question"]}
-                options={giveOptions(data[name_chapter][qes])}
-                answer={data[name_chapter][qes]["Answer"][0]}
-              />
-            )
-        ))}
-      </div>
-    </div>
-  );
+    const [isShowingHistory, setShowingHistory] = useState(false);
+    function giveOptions(json: any) {
+        const hi = {
+            "a": json["a"],
+            "b": json["b"],
+            "c": json["c"],
+            "d": json["d"],
+            "e": json["e"],
+        };
+        return hi;
+    }
+    let data: any = Data;
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>{name_chapter}</h1>{" "}
+            <button
+                style={{
+                    marginRight: "auto",
+                    marginLeft: "auto",
+                    backgroundColor: "tan",
+                }}
+                onClick={() => {
+                    setShowingHistory(!isShowingHistory);
+                }}
+            >
+                Show previous
+            </button>
+            <div style={{ width: "50%", margin: "auto" }}>
+                {Object.keys(data[name_chapter]).map((qes, i) => (
+                    !isShowingHistory
+                        ? (
+                            <>
+                                <Question
+                                    key={i}
+                                    q_num={qes}
+                                    question={data[name_chapter][qes]["Question"]}
+                                    options={giveOptions(data[name_chapter][qes])}
+                                    answer={data[name_chapter][qes]["Answer"][0]}
+                                />
+                            </>
+                        )
+                        : (
+                            <LastQuestion
+                                key={i}
+                                question={data[name_chapter][qes]["Question"]}
+                                options={giveOptions(data[name_chapter][qes])}
+                                answer={data[name_chapter][qes]["Answer"][0]}
+                            />
+                        )
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Screen;
